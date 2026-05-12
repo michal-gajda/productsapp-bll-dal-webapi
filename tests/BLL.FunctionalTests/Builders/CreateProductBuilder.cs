e4bbd303eafd511d8e2c936493382207062f91d1
Michal Gajda <michal@gajda.co.uk>
2026-05-12 07:35:50 +0200

    Add Builder

---
+++ tests/BLL.FunctionalTests/Builders/CreateProductBuilder.cs
@@ -0,0 +1,61 @@
+﻿namespace BLL.FunctionalTests.Builders;
+
+using BLL.Models;
+
+public sealed class CreateProductBuilder
+{
+    private string name = "Test Product";
+    private string? description = default;
+    private decimal price;
+    private int stockQuantity;
+    private string category = "Default Category";
+
+    private CreateProductBuilder() { }
+
+    public static CreateProductBuilder CreateProductDefault()
+    {
+        return new CreateProductBuilder();
+    }
+
+    public CreateProductBuilder WithCategory(string category)
+    {
+        this.category = category;
+        return this;
+    }
+
+    public CreateProductBuilder WithDescription(string? description)
+    {
+        this.description = description;
+        return this;
+    }
+
+    public CreateProductBuilder WithName(string name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    public CreateProductBuilder WithPrice(decimal price)
+    {
+        this.price = price;
+        return this;
+    }
+
+    public CreateProductBuilder WithStockQuantity(int stockQuantity)
+    {
+        this.stockQuantity = stockQuantity;
+        return this;
+    }
+
+    public CreateProduct Build()
+    {
+        return new CreateProduct
+        {
+            Name = this.name,
+            Description = this.description,
+            Price = this.price,
+            StockQuantity = this.stockQuantity,
+            Category = this.category,
+        };
+    }
+}
--- tests/BLL.FunctionalTests/ProductServiceTests.cs
+++ tests/BLL.FunctionalTests/ProductServiceTests.cs
@@ -1,6 +1,6 @@
 ﻿namespace BLL.FunctionalTests;
 
-using BLL.Models;
+using BLL.FunctionalTests.Builders;
 using BLL.Services;
 
 [TestClass]
@@ -11,33 +11,32 @@ public sealed class ProductServiceTests : TestBase
     {
         var productService = this.ServiceProvider.GetRequiredService<IProductService>();
 
-        var model = new CreateProduct
-        {
-            Name = "Test Product",
-            Description = "This is a test product.",
-            Price = 9.99m,
-            Category = "Test Category",
-            StockQuantity = 100,
-        };
+        var model = CreateProductBuilder.CreateProductDefault()
+            .WithCategory("Test Category")
+            .WithDescription("This is a test product.")
+            .WithName("Test Product")
+            .WithPrice(9.99m)
+            .WithStockQuantity(100)
+            .Build();
 
         var (_, product) = await productService.CreateProductAsync(model);
 
         product.ShouldNotBeNull();
 
-        product.Name
-            .ShouldBe(model.Name)
+        product.Category
+            .ShouldBe(model.Category)
             ;
 
         product.Description
             .ShouldBe(model.Description)
             ;
 
-        product.Price
-            .ShouldBe(model.Price)
+        product.Name
+            .ShouldBe(model.Name)
             ;
 
-        product.Category
-            .ShouldBe(model.Category)
+        product.Price
+            .ShouldBe(model.Price)
             ;
 
         product.StockQuantity
